1adbc64ee8c89eef5df2b90bc2be19f11a6a5e98
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2025-02-17 05:45:37 -0300

    created automatic association via e-mail, authentication through password route using jwt and created route to get profile of user that is logged in

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -27,6 +27,15 @@ export async function createAccount(app: FastifyInstance) {
       return reply.status(400).send({ message: "user with same e-mail already exists." })
     }
 
+    const [, domain] = email.split('@')
+
+    const autoJoinOrganization = await prisma.organization.findFirst({
+      where: {
+        domain,
+        shouldAttachUsersByDomain: true,
+      }
+    })
+
     const passwordHash = await hash(password, 6)
 
     await prisma.user.create({
@@ -34,6 +43,11 @@ export async function createAccount(app: FastifyInstance) {
         name,
         email,
         passwordHash,
+        member_on: autoJoinOrganization ? {
+          create: {
+            organizationId: autoJoinOrganization.id,
+          },
+        } : undefined,
       }
     })
 
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,6 +1,7 @@
 import {fastify} from 'fastify'
 import fastifyCors from '@fastify/cors'
 import fastifySwagger from '@fastify/swagger'
+import fastifyJwt from '@fastify/jwt'
 import fastifySwaggerUI from '@fastify/swagger-ui'
 import { jsonSchemaTransform,
   serializerCompiler,
@@ -8,6 +9,8 @@ import { jsonSchemaTransform,
   ZodTypeProvider
  } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
+import { getProfile } from './routes/auth/get-profile'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -30,9 +33,15 @@ app.register(fastifySwaggerUI, {
   routePrefix: '/docs',
 })
 
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
 app.register(fastifyCors)
 
 app.register(createAccount)
+app.register(authenticateWithPassword)
+app.register(getProfile)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
