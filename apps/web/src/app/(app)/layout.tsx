57bd2884f76af145eb572138dffbba701cfa6ae4
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2025-03-09 06:47:07 -0300

    i have made the create organization page then created the funcionality to add the logic of creating organization then i dived into the concept of parallel routes on next and interception routes too

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -3,18 +3,21 @@ import { redirect } from "next/navigation";
 
 export default async function AppLayout({
   children,
+  sheet,
 }: Readonly<{
   children: React.ReactNode;
+  sheet: React.ReactNode;
 }>) {
   if (!isAuthenticated()) {
   redirect('/auth/sign-in')
   }
-
+  
   return (
-  <>  
+  <>
+  {children}
+  {sheet}
+  </>
+  )
 
-        {children}
 
-        </>  
-  );
 }
--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -1,13 +1,14 @@
 import { Header } from "@/components/header";
 
-
 export default async function Projects() {
 
 
   return (
-   <div className="py-4">
-    <Header />
-    <main></main>
-   </div>
+    <div className="py-4 space-y-4">
+        <Header />
+        <main className="mx-auto w-full max-w-[1200px]">
+        Projects
+        </main>
+       </div>
   );
 }
