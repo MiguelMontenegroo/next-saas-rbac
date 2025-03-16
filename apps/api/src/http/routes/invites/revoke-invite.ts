6d236cf691baafdeaa699a3cadbff4c4283b9d70
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2025-03-16 06:48:09 -0300

    created feature to update member role on organization, list invites and to create new invites

--- apps/api/src/http/routes/invites/revoke-invite.ts
+++ apps/api/src/http/routes/invites/revoke-invite.ts
@@ -8,7 +8,7 @@ import { UnauthorizedError } from "../_errors/unauthorized-error";
 import { BadRequestError } from "../_errors/bad-request-error";
 
 export async function revokeInvite(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().register(auth).post('/organizations/:slug/invites/:inviteId', {
+  app.withTypeProvider<ZodTypeProvider>().register(auth).delete('/organizations/:slug/invites/:inviteId', {
     schema: {
       tags: ['invites'],
       summary: 'Revoke aninvite',
